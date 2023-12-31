f3c12a0e97b2df4a0211b81344828aa5d08dc8f9
yuberee <59583743+yuberee@users.noreply.github.com>
2023-12-31 22:23:13 +0100

    MergeComponent, ICollisionListener

---
+++ code/MergeComponent.cs
@@ -0,0 +1,49 @@
+using Sandbox;
+using System.Linq;
+using static Sandbox.Component;
+
+public sealed class MergeComponent : Component, ICollisionListener
+{
+	/// <summary>
+	/// This will only merge with others that have the same merge order and add 1 to it
+	/// </summary>
+	[Property]
+	public int MergeOrder { get; set; } = 1;
+
+	public Collider Collider { get; set; }
+	public Rigidbody Rigidbody { get; set; }
+
+	protected override void OnStart()
+	{
+		base.OnStart();
+
+		Collider = GameObject.Components.Get<Collider>();
+		Rigidbody = GameObject.Components.Get<Rigidbody>();
+	}
+
+	protected override void OnUpdate()
+	{
+		base.OnUpdate();
+
+	}
+
+	public void OnCollisionStart( Collision other )
+	{
+		var otherCollider = other.Other.Collider;
+		var otherObject = otherCollider.GameObject;
+		var otherMerger = otherObject.Components.Get<MergeComponent>();
+
+		if ( otherMerger == null ) return;
+
+		Log.Info( "HII" );
+	}
+
+	public void OnCollisionUpdate( Collision other )
+	{
+	}
+
+	public void OnCollisionStop( CollisionStop other )
+	{
+
+	}
+}
