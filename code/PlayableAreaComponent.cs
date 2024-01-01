261adfa8f694d3bd84407356f3ee552891299c2a
yuberee <59583743+yuberee@users.noreply.github.com>
2024-01-01 21:13:06 +0100

    Local parameter to IsInside...

--- code/PlayableAreaComponent.cs
+++ code/PlayableAreaComponent.cs
@@ -18,21 +18,41 @@ public sealed class PlayableAreaComponent : Component
 		draw.LineBBox( PlaceableBounds );
 	}
 
+	/// <summary>
+	/// Transform a point into the local system
+	/// </summary>
+	/// <param name="point"></param>
+	/// <returns></returns>
 	public Vector3 WorldToLocal( Vector3 point ) => Transform.World.PointToLocal( point );
 
 	/// <summary>
 	/// Check if the point is inside of the defined playable bounds
 	/// </summary>
 	/// <param name="point"></param>
+	/// <param name="local"></param>
 	/// <returns></returns>
-	public bool IsInsidePlayableBounds( Vector3 point ) => PlayableBounds.Contains( WorldToLocal( point ) );
+	public bool IsInsidePlayableBounds( Vector3 point, bool local = false ) => PlayableBounds.Contains( local ? point : WorldToLocal( point ) );
 
 	/// <summary>
 	/// Check if the point is inside of the defined placeable bounds
 	/// </summary>
 	/// <param name="point"></param>
+	/// <param name="local"></param>
 	/// <returns></returns>
-	public bool IsInsidePlaceableBounds( Vector3 point ) => PlaceableBounds.Contains( WorldToLocal( point ) );
+	public bool IsInsidePlaceableBounds( Vector3 point, bool local = false ) => PlaceableBounds.Contains( local ? point : WorldToLocal( point ) );
+
+	/// <summary>
+	/// Gets the mouse position of the main camera
+	/// </summary>
+	/// <returns></returns>
+	public Vector3 GetMousePosition()
+	{
+		var screenRay = Camera.Main.GetRay( Mouse.Position );
+		var screenPosition = Scene.Trace.Ray( screenRay, 9999f )
+			.Run();
+
+		return screenPosition.HitPosition;
+	}
 
 	protected override void OnStart()
 	{
@@ -44,10 +64,7 @@ public sealed class PlayableAreaComponent : Component
 	{
 		base.OnUpdate();
 
-		var screenRay = Camera.Main.GetRay( Mouse.Position );
-		var screenPosition = Scene.Trace.Ray( screenRay, 9999f )
-			.Run();
-		Log.Info( screenPosition.HitPosition );
-		Log.Info( IsInsidePlayableBounds( screenPosition.HitPosition ) );
+		Log.Info( $"Is inside playable? {IsInsidePlayableBounds( GetMousePosition() )}" );
+		Log.Info( $"Is inside placeable? {IsInsidePlaceableBounds( GetMousePosition() )}" );
 	}
 }
